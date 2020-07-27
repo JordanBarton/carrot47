9c753b03af0c839dd3852495620b986a6052f608
jordanbarton97 <58301908+jordanbarton97@users.noreply.github.com>
2020-07-27 14:19:19 +0200

    Update platform_zeros.py

--- python_projects/platform_zeros.py
+++ python_projects/platform_zeros.py
@@ -1,56 +1,3 @@
-#API_testing
-import requests
-import csv
-import email_notification
-#%%%
-
-URL = "http://api.open-notify.org" 
-end_point1 = "/iss-now.json"
-end_point2 = "/iss-pass.json" #needs latlong to be specified
-end_point3 = "/astros.json"
-
-request = requests.get(URL + end_point3)
-print(request.status_code)
-print(request.text)
-request_json = request.json()
-print(request_json)
-number = request_json["number"]
-print(number)
-for person in request_json["people"]:
-    print(person["name"])
-
-
-
-
-
-
-#words is end point by using ?
-#according to documentation rel rhy = x finds words that rhyme with x
-#URL = "https://api.datamus.com/words?rel rhy=jingle"
-#json = -> : 
-parameter = {"rel_rhy":"jingle"}
-request = requests.get("https://api.datamuse.com/words" , parameter)
-print(request.text)
-rhyme = []
-request_json = request.json()
-for entry in request_json:
-    rhyme.append(entry["word"])
-print(rhyme)
-
-
-
-
-#from twilio.rest import Client
-
-#account_sid = 'AC61c01b3475f3ee650e9f6873e4368fe4'
-#auth_token = '5ef76f1eb6f1807a9c0fcceb37f6333d'
-#client = Client(account_sid , auth_token)
-
-#msg = "your code has finished"
-    
-#message = client.messages.create(to = '+447825915787',
-                                 #from_ = '+447401273046',
-                                 #body = msg)
 
 #%%
 import requests
@@ -72,8 +19,8 @@ format_date(datetime.date(2020,10,10))
 print("\n\n\n\n\n")
 URL = "http://api.rtt.io/api/v1/json/search/"
 
-api_login = "rttapi_spudsinhell"
-api_password = "2fc2475a7ec34500600ec03a8dc34447d0163522"
+api_login = "rttapi_XXXXXXXXXXXX"
+api_password = "XXXXXXXXXXXXXXXXXXXXX"
 #data_get = {str(api_login) : str(api_password)}
 zero_stations = []
 codes = []
